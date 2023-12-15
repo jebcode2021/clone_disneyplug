e35b926863fd52b70da7a81e30dbb3f06f71de5b
jebcode2021 <jebcode@outlook.com.br>
2023-12-15 07:31:32 -0300

    Desafio

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/scss/*.scss')
@@ -7,7 +8,15 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function img() {
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+}
+
+exports.default = gulp.parallel(styles, img);
+exports.img = img;
+
 exports.watch = function () {
     gulp.watch('./src/scss/*.scss', gulp.parallel(styles));
 };
