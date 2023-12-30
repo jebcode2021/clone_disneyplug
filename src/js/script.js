58aab5fb4da91e060101e22ab289070b086130dd
jebcode2021 <jebcode@outlook.com.br>
2023-12-30 16:22:38 -0300

    loader

--- src/js/script.js
+++ src/js/script.js
@@ -67,3 +67,13 @@ function escondeTodasAbas() {
     }
 
 }
+
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+
+    loader.classList.add("loader--hidden");
+
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild(loader);
+    });
+});
